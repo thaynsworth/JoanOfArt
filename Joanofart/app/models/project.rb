0eee8487d3dba239fcee7e17df9b718dc0697efb
thaynsworth <thomas.haynsworth@gmail.com>
2015-01-22 13:28:00 -0500

    added paperclip to models

--- Joanofart/app/models/project.rb
+++ Joanofart/app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates :name, :type, presence: true
-  validates_attachment_content_type :image, :content_type => ["image/jpg","image/jpeg","image/png"]
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg","image/jpeg","image/png"]
 end
