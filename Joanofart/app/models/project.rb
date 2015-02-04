e6178633e12d9662507b854ea7035593a838f192
thaynsworth <thomas.haynsworth@gmail.com>
2015-02-04 12:46:43 -0500

    updated project index format

--- Joanofart/app/models/project.rb
+++ Joanofart/app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "500x500", :medium => "300x300>", :thumb => "250x250>" }, :default_url => "/images/:style/missing.png"
 
   validates :name, :category, presence: true
   validates_attachment_content_type :avatar, :content_type => ["image/jpg","image/jpeg","image/png"]
