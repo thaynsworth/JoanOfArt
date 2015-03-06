98a8e74e4b2513dbe5ae51e2964594be4d2341bb
thaynsworth <thomas.haynsworth@gmail.com>
2015-03-06 14:38:59 -0500

    added contact view

--- Joanofart/config/routes.rb
+++ Joanofart/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
