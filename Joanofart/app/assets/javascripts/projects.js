d1aec3f48b2030dffc9a9d872a4003474b6fa5cd
thaynsworth <thomas.haynsworth@gmail.com>
2015-03-06 02:13:29 -0500

    project-nav fade in/out

--- Joanofart/app/assets/javascripts/projects.js
+++ Joanofart/app/assets/javascripts/projects.js
@@ -1,13 +1,24 @@
 function emptyProjects(){
-	console.log('Hide all projects')
+	console.log('Hide all projects');
 
 	$("div.project-load").hide();
 	$("div.animals").hide();
 	$("div.flowers").hide();
+
+	$('#project-nav li').css({
+		'border-bottom': 'none',
+		'color': 'black'
+	})
+
 }
 
 function animalProjects(){
 	var animals = $('.animals');
+	var animalButton = $('#animal-button');
+	animalButton.css({
+		'border-bottom': '3px solid grey',
+		'color': 'grey'
+	});
 	animals.fadeIn(1000, function(){
 		animals.show();
 	});
@@ -16,6 +27,11 @@ function animalProjects(){
 
 function flowerProjects(){
 	var flowers = $('.flowers');
+	var flowerButton = $('#flower-button');
+	flowerButton.css({
+		'border-bottom': '3px solid grey',
+		'color': 'grey'		
+	})
 	flowers.fadeIn(1000, function(){
 		flowers.show();
 	});
