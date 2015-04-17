82d417289a02db38f62bbb601b0f5ad772212a53
thaynsworth <thomas.haynsworth@gmail.com>
2015-04-17 14:14:06 -0400

    updated project index ids

--- Joanofart/app/assets/javascripts/projects.js
+++ Joanofart/app/assets/javascripts/projects.js
@@ -1,10 +1,10 @@
 
 function emptyProjects(){
 	console.log('Hide all projects');
-	$('#cssmenu li').removeClass('active');
-	$("div.project-load").hide();
-	$("div.animals").hide();
-	$("div.flowers").hide();
+	$('#project-nav li').removeClass('active');
+	$("div#project-load").hide();
+	$("div#animals").hide();
+	$("div#flowers").hide();
 
 }
 
@@ -12,19 +12,19 @@ function updateProjects(obj) {
 	emptyProjects();
 	var topic = obj.attr('id');
 	if (topic === 'project-main-button') {
-		var main = $('.project-load');
-		main.fadeIn(1000, function(){
+		var main = $('#project-load');
+		main.fadeIn(400, function(){
 			main.show();
 		})
 	} else if (topic === 'flower-button') {
-		var flowers = $('.flowers');
-		flowers.fadeIn(1000, function(){
+		var flowers = $('#flowers');
+		flowers.fadeIn(400, function(){
 			flowers.show();
 		});
 		console.log('showing flowers');		
 	} else if (topic === 'animal-button') {
-		var animals = $('.animals');
-		animals.fadeIn(1000, function(){
+		var animals = $('#animals');
+		animals.fadeIn(400, function(){
 			animals.show();
 		});
 		console.log('showing animals');
