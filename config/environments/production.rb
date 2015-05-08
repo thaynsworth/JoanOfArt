358338f23490959a80a2c8636b258c8d2a18f307
thaynsworth <thomas.haynsworth@gmail.com>
2015-05-08 16:24:45 -0400

    added key connetions to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  AWS::S3::Base.establish_connection!(
+   :access_key_id   => ENV['S3_KEY'],
+   :secret_access_key => ENV['S3_SECRET']
+  )
 end
