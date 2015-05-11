cb1d8b4b9994bb3ee32ef1f25ccc90755a73d056
thaynsworth <thomas.haynsworth@gmail.com>
2015-05-11 14:18:37 -0400

    created new bucket in east region

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,8 +13,7 @@ class Project < ActiveRecord::Base
     {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_endpoint => 's3-eu-west-2.amazonaws.com'
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   end
 end
