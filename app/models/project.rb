46a038a07252e65c056144ddc466866b8b3340a4
thaynsworth <thomas.haynsworth@gmail.com>
2015-05-11 13:34:09 -0400

    updated aws key id name

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,8 +12,8 @@ class Project < ActiveRecord::Base
   def s3_credentials
     {
       :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   end
 end
